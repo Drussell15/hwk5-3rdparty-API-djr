dae5726c27fc012663673e130d54131a59600618
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-05-03 03:54:06 -0700

    vs code kept messing up- finished in sublimetext
    vs code kept crashing, and was returning errors left and right that really were confusing because my partners looked at the code and said it looked fine to them. I used another editor and low and behold everything worked.

---
+++ script.js
@@ -0,0 +1,105 @@
+// variable for holding all time block text 9AM-5PM
+var timeBlocksText = ["", "", "", "", "", "", "", "", ""];
+//needed to access timeblcos for event delegations
+var timeBlocks = document.getElementById("timeBlocks");
+// current hour for text area coloring
+var currentHour = moment().format("H");
+
+// calls initialization
+initialize();
+
+// function to start page
+function initialize() {
+    // sets current date in currentDay p
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+    loadSchedule();
+    renderTimeBlocks();
+}
+
+// renders time blocks
+function renderTimeBlocks() {
+    // clears time blocks
+    $("#timeBlocks").empty();
+
+    for (var i = 0; i < 9; i++) {
+        // sets a variable for time of current block rendered
+        var time = i + 9;
+        if (time > 12) {
+            time -= 12;
+        }
+
+        //creates time block and adds classes
+        var timeBlock = $("<div>");
+        timeBlock.addClass("time-block row");
+
+        // creates hour div and adds classes/text content
+        var hourDiv = $("<div>");
+        hourDiv.addClass("hour col-2 col-md-1");
+        if (time > 8) {
+            hourDiv.text(time + "AM");
+        }
+        else {
+            hourDiv.text(time + "PM")
+        }
+
+        // creates text area add classes, index and value
+        var textArea = $("<textarea>").addClass("col-8 col-md-10");
+        if ((i + 9) < currentHour) {
+            textArea.addClass("past");
+        }
+        else if ((i + 9) == currentHour) {
+            textArea.addClass("present");
+        }
+        else {
+            textArea.addClass("future");
+        }
+        textArea.attr("index", i);
+        textArea.val(timeBlocksText[i]);
+
+        // creates save button
+        var saveButton = $("<button>").attr("type", "button");
+        saveButton.attr("index", i);
+        saveButton.addClass("saveBtn col-2 col-md-1");
+
+        // creates save button icon add appends it to save button
+        var saveIcon = $("<i>").addClass("far fa-save");
+        saveButton.append(saveIcon);
+
+        // appends elements to timeBlock
+        timeBlock.append(hourDiv, textArea, saveButton);
+
+        // appends new time block to the page
+        $("#timeBlocks").append(timeBlock);
+    }
+}
+
+// adds event listener to save T-Block
+timeBlocks.addEventListener("click", function (event) {
+    if (event.target.type === "button") {
+        var index = event.target.getAttribute("index");
+        var content = event.target.parentElement.children[1].value;
+        timeBlocksText.splice(index, 1, content);
+        saveSchedule();
+    }
+    else if (event.target.parentElement.type === "button") {
+        var index = event.target.parentElement.getAttribute("index");
+        var content = event.target.parentElement.parentElement.children[1].value;
+        timeBlocksText.splice(index, 1, content);
+        saveSchedule();
+    }
+});
+
+// saves the loaded schedule to localStorage
+function saveSchedule() {
+    localStorage.setItem("timeBlocksText", JSON.stringify(timeBlocksText));
+}
+// loads schedule from local storage
+function loadSchedule() {
+    var tempLoad = JSON.parse(localStorage.getItem("timeBlocksText"));
+
+    if (tempLoad !== null) {
+        timeBlocksText = tempLoad;
+    }
+}
+//Dale Russell hwk5
